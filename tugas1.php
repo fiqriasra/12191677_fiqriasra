348055a91836354382d48830ecf47ff849ed23f5
fiqriasra <62532168+fiqriasra@users.noreply.github.com>
2021-03-15 16:38:19 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,6 @@
+tugas1
+<?php
+   $ikanku = array("Teri","lele","bawal","tuna");
+   string : "ikan teri sering di jumpai di nasi kucing angkringan dan saya konsumsi pas tangal tua. kalau tanggal muda sering kuliner lele, bawal, dan tuna"
+<script>nama : Muhammad fiqri asra
+nim : 12191677</script>
